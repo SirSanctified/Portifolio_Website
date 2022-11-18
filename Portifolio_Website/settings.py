4e2b76558be7327ed34a424f1f712ac2adc2e838
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-11-18 17:32:49 +0200

    Edit settings.py

--- Portifolio_Website/settings.py
+++ Portifolio_Website/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'web-production-b285.up.railway.app', 'pritchardmambambo.tech']
 
@@ -78,24 +78,13 @@ WSGI_APPLICATION = 'Portifolio_Website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'Portifolio_Website',
-            'USER': os.getenv('USERNAME'),
-            'PASSWORD': os.getenv('PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
